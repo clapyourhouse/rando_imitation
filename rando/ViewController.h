a9ef5e3c969029b41c400b2ad4d32872dd9f6a83
Shogo Kitamura <Kitamura@infocity.co.jp>
2015-04-03 18:31:59 +0900

    Initial Commit

---
+++ rando/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rando
+//
+//  Created by KitamuraShogo on 2015/04/03.
+//  Copyright (c) 2015年 ShogoKitamura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
